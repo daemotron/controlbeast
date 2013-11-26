91c6c00a16252b2a43a1017ee17b7f6f0302e4be
Daemotron <mail@daemotron.net>
2013-11-26 20:54:58 +0100

    Implement simple mechanism for configuration handling

--- controlbeast/__init__.py
+++ controlbeast/__init__.py
@@ -10,6 +10,24 @@
 VERSION = (0, 1, 0, 'alpha', 0)
 COPYRIGHT = ('2013', 'the ControlBeast team')
 
+DEFAULTS = {
+    'scm': 'Git'
+}
+
+
+def get_conf(key):
+    """
+    Get a global configuration value by its key
+
+    :param key: string identifying the requested configuration value
+    :returns the requested configuration value or None
+    """
+    if key in DEFAULTS:
+        return DEFAULTS[key]
+    else:
+        return None
+
+
 def get_version(*args, **kwargs):
     """
     Returns PEP 386 compliant version number for the Pytain package
