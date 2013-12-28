53e4fdb7738618e1b080d8a50377cbf01d2fbed9
daemotron <mail@daemotron.net>
2013-12-28 20:06:04 +0100

    Fixed module docstring

--- test/t_controlbeast/t_scm/test_CbSCMExceptions.py
+++ test/t_controlbeast/t_scm/test_CbSCMExceptions.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-    test.t_controlbeast.t_scm.test_Exceptions
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    test.t_controlbeast.t_scm.test_CbSCMExceptions
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     :copyright: Copyright 2013 by the ControlBeast team, see AUTHORS.
     :license: ISC, see LICENSE for details.
