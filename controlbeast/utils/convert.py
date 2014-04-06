0e427be8b46ec4a817fba88d315a99e00388852d
Daemotron <mail@daemotron.net>
2014-04-06 09:20:28 +0200

    Refactored convert utility to use the global configuration instead of a hard-coded default charset.

--- controlbeast/utils/convert.py
+++ controlbeast/utils/convert.py
@@ -6,6 +6,7 @@
     :copyright: Copyright 2013 by the ControlBeast team, see AUTHORS.
     :license: ISC, see LICENSE for details.
 """
+from controlbeast.conf import get_conf
 
 
 def to_bytes(value):
@@ -17,7 +18,7 @@ def to_bytes(value):
     """
     if isinstance(value, str):
         # noinspection PyArgumentList
-        return bytes(source=value, encoding='utf-8', errors='replace')
+        return bytes(source=value, encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
     elif isinstance(value, bytearray):
         return bytes(value)
     elif isinstance(value, bytes):
@@ -26,7 +27,7 @@ def to_bytes(value):
         return b''
     else:
         # noinspection PyArgumentList
-        return bytes(source=str(value), encoding='utf-8', errors='replace')
+        return bytes(source=str(value), encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
 
 
 def to_str(value):
@@ -37,7 +38,7 @@ def to_str(value):
     :return: Resulting string
     """
     if isinstance(value, bytes) or isinstance(value, bytearray):
-        return value.decode(encoding='utf-8', errors='replace')
+        return value.decode(encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
     elif value is None:
         return ''
     else:
